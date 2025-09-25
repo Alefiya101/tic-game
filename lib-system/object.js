d811932178e54f1db8b722cf6c80b332e258f51e
Alefiya101 <154125220+Alefiya101@users.noreply.github.com>
2025-09-25 23:54:29 +0530

    Delete lib-system directory

--- lib-system/object.js
+++
@@ -1,9 +0,0 @@
-class Myobject{
-    constructor(name,roll,line){
-        this.name=name;
-        this.roll=roll;
-        this.line=line;
-    }}
-
-    let obj=new Myobject("alu",13,"hey");
-    console.log(obj);
